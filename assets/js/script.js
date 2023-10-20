9be47771725dc68d568e749f4390cc2042124603
Jack Tubby <jack200034@hotmail.com>
2023-10-20 11:48:41 +0100

    Create load dialogue functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,22 @@ const loadStoryFromJson = async (story) => {
     const response = await fetch(`assets/js/stories/${story}.json`);
     const res = await response.json();
     loadDialogue(res);
-}
+};
 
-const loadDialogue = (story) => {
-}
-
+/**
+ * Extracts the dialogue of the current scene from the provided story object.
+ * Assumes a default starting scene if no other scene is specified.
+ *
+ * @param {Object} story - The story object loaded from the JSON.
+ */
+const loadDialogue = (story, scene = "scene_one") => {
+    const dialogue = story[scene]?.dialogue;
+    const storyboard = document.querySelector('#storyboard'); // update this once story board is created
+    if (dialogue) {
+        storyboard.textContent = dialogue;
+    } else {
+        console.error("Dialogue for the current scene not found.");
+    }
+};
 // Test it works
-loadStoryFromJson('desert-island');
+loadStoryFromJson("desert-island");
